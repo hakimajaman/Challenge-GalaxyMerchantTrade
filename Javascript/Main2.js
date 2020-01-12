556da18a9ba31b6bf6196b9bf13a6f96955d966c
Hakim <robotorwhat@gmail.com>
2020-01-12 22:13:22 +0700

    regexCredits

--- Javascript/Main2.js
+++ Javascript/Main2.js
@@ -183,6 +183,18 @@ const planet = (language) => {
  *
  */
   var regexIt = /^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/
+  var romanListValue = [1,5,10,50];
+
+  function changeChar2(change){
+    var values = -1
+    switch(change){
+      case 'I': values = romanListValue[0]; break;
+      case 'V': values = romanListValue[1]; break;
+      case 'X': values = romanListValue[2]; break;
+      case 'L': values = romanListValue[3]; break;
+    }
+    return values;
+  }
 
 /*
  *
@@ -191,29 +203,99 @@ const planet = (language) => {
  */
   var regexIs = /^([a-zA-Z]+) is (I|V|X|L|C|D|M)$/
 
-  var planetLanguageValue;
-  var input = language;
-  var MatchIt = regexIs.test(input);
+  var planetLanguageWords = [];
+  var planetLanguageValues = [];
+  var inputLanguage = language;
+  var MatchItLanguage = regexIs.test(inputLanguage);
 
-  if(MatchIt == false){
-    console.log("Read README.md for how to use it");
+  if(MatchItLanguage == false){
+          console.log("Read README.md for how to use it");
     return;
   } else {
-    planetLanguageValue = input.match(regexIs);
-    console.log(planetLanguageValue);
-    console.log("----\n");
+    var matcherLanguage = inputLanguage.match(regexIs);
+    planetLanguageWords.push(matcherLanguage[1]);
+    planetLanguageValues.push(matcherLanguage[2]);
+          console.log(planetLanguageWords);
+          console.log(planetLanguageValues);
+          //console.log(matcherLanguage);
   }
 
+  var changCharac = "";
+  for(var i=0; i<planetLanguageValues.length; i++) {
+    changCharac += changeChar2(planetLanguageValues[i]);
+  }
+          console.log(changCharac);
+ 
+  function planetChangeChar(change){
+    for(var i in planetLanguageValues){
+      var values = ""
+      switch(change){
+        case planetLanguageWords[i] : values = planetLanguageValues[i]; break;
+      }
+      return values;
+    }
+  }
+  //planetChangeChar("glob");
+
+          console.log("----END STORING NUMBERS------\n");
 /*
  *
  * this for storing credits
- * regex = /http\:\/\/www\.youtube\.com\/watch\?v=([\w-]{11})/;
+ * 
  */
-  //var regexCredits = /^([a-zA-Z]+) is )$/
-var regex = /http\:\/\/www\.youtube\.com\/watch\?v=([\w-]{11})/;
-var url = 'http://www.youtube.com/watch?v=Ahg6qcgoay4';
-var id = url.match(regex);
-  console.log(id);
+  var regexCredits = /^([a-zA-Z]+)([a-zA-Z\s]*) is ([0-9]+) (Credits)$/
+  //var regexCreditsBETA = /^([0-9]+)([a-zA-Z\s]*) is ([0-9]+) (Credits)$/
+
+  var planetCreditsWords = [];
+  var planetCreditsValues = 0;
+  var planetCreditsRomanValues = "";
+  var planetCreditsCountRomanValues = [];
+  var planetCreditsCount = "";
+  var inputCredits = "glob glob Silver is 34 Credits";
+  var MatchItCredits = regexCredits.test(inputCredits);
+
+  if(MatchItCredits == false){
+    console.log("Please Read README.md for how to use it");
+    return;
+  } else {
+    var matcherCredits = inputCredits.match(regexCredits);
+    var aftersplitCredits = matcherCredits[2].split(" ");
+    var lastArrCredits = aftersplitCredits[aftersplitCredits.length-1];
+    planetCreditsWords.push(lastArrCredits);
+    planetCreditsValues = matcherCredits[3];
+    planetCreditsCount += matcherCredits[1]; planetCreditsCount += matcherCredits[2];
+    planetCreditsCount = planetCreditsCount.split(" ");
+    planetCreditsCount.pop();
+          console.log(planetCreditsWords);
+          console.log(planetCreditsValues);
+
+  }
+  var index = 0;
+
+  for(var i=0; i<planetCreditsCount.length; i++){
+    for(var j in planetLanguageWords){
+      index = planetCreditsCount[i].indexOf(planetLanguageWords[j])
+      while(index == -1){
+          console.log("Your Planet Word was not found");
+      return;
+      }
+      planetCreditsRomanValues += planetChangeChar(planetCreditsCount[i]);
+    }
+  }
+
+  var countPlanetCreditsValue1 = 0;
+  for(var i=0; i<planetCreditsRomanValues.length; i++) {
+    countPlanetCreditsValue1 += changeChar2(planetCreditsRomanValues.charAt(i));
+  }
+  var countPlanetCreditsValue2 = planetCreditsValues / countPlanetCreditsValue1;
+  planetCreditsCountRomanValues.push(countPlanetCreditsValue2);
+
+          console.log(planetCreditsCountRomanValues);        
+          console.log(countPlanetCreditsValue1); 
+          console.log(planetCreditsRomanValues);
+          console.log(planetCreditsCount);
+          console.log(MatchItCredits);
+          console.log("----END STORING CREDITS------\n");
 }
 
 //planet("glob prok");
